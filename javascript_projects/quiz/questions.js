0a0b9c74def79d48acee0e83361be3f9996d3269
ashir245 <117296966+ashir245@users.noreply.github.com>
2024-01-31 15:01:05 +0300

    added quiz

---
+++ javascript_projects/quiz/questions.js
@@ -0,0 +1,52 @@
+let questions = [
+  {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "C. Hyper Text Markup Language",
+    options: [
+      "A. Hyper Type Multi Lanaguage",
+      "B. Hyper Text Multiple Lanaguage",
+      "C. Hyper Text Markup Language",
+      "D. Hyper Text Multi Language",
+    ],
+  },
+  {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "A.Cascading style sheet",
+    options: [
+      "A.Cascading style sheet",
+      "B.Cute style sheet",
+      "C.Computer style sheet",
+      "D.Cas style sheet",
+    ],
+  },
+  {
+    numb: 3,
+    question: "What does PHP stand for?",
+    answer: "A. Hypertext Preprocessor",
+    options: [
+      "A. Hypertext Preprocessor",
+      "B. Pretext Hypertext Preprocessor",
+      "C. Personal Home Processor",
+      "D. None of the above",
+    ],
+  },
+  {
+    numb: 4,
+    question: "Variable name in PHP starts with?",
+    answer: "B.$ (Dollar)",
+    options: [
+      "A.! (Exclamation)",
+      "B.$ (Dollar)",
+      "C.& (Ampersand)",
+      "D.# (Hash)",
+    ],
+  },
+  {
+    numb: 5,
+    question: "Which of the following is not a variable scope in PHP?",
+    answer: "A.Extern",
+    options: ["A.Extern", "B.Local", "C.Static", "D.Global"],
+  },
+];
---
+++ javascript_projects/quiz/quiz.js
@@ -0,0 +1,137 @@
+const startBtn = document.querySelector(".btn");
+const popInfo = document.querySelector(".popup-info");
+const exitbtn = document.querySelector(".exit-btn");
+const main = document.querySelector(".main");
+const continuebtn = document.querySelector(".continue-btn");
+const quizsection = document.querySelector(".quiz-section");
+const quizbox = document.querySelector(".quiz-box"); // Changed selector here
+const resultbox = document.querySelector(".result-box");
+const tryagainbtn = document.querySelector(".try-again-btn");
+const gohomebtn = document.querySelector(".gohome-btn");
+startBtn.onclick = () => {
+  popInfo.classList.add("active");
+  main.classList.add("active");
+};
+
+exitbtn.onclick = () => {
+  popInfo.classList.remove("active");
+  main.classList.remove("active");
+};
+
+continuebtn.onclick = () => {
+  quizsection.classList.add("active");
+  popInfo.classList.remove("active");
+  main.classList.remove("active");
+  quizbox.classList.add("active");
+  showQuestions(0);
+  questionCount(1);
+  headerscore();
+};
+tryagainbtn.onclick = () => {
+  quizbox.classList.add("active");
+  resultbox.classList.remove("active");
+  questionCount = 0;
+  questionNumb = 1;
+  userscore = 0;
+  showQuestions(questionCount);
+  countQuestion(questionNumb);
+  nextBtn.classList.remove("active");
+  headerscore();
+};
+gohomebtn.onclick = () => {
+  quizsection.classList.remove("active");
+  nextBtn.classList.remove("active");
+  resultbox.classList.remove("active");
+  questionCount = 0;
+  questionNumb = 1;
+  userscore = 0;
+  showQuestions(questionCount);
+  countQuestion(questionNumb);
+  headerscore();
+};
+
+let questionCount = 0;
+let questionNumb = 1;
+let userscore = 0;
+const nextBtn = document.querySelector(".next-btn");
+nextBtn.onclick = () => {
+  if (questionCount < questions.length - 1) {
+    questionCount++;
+    showQuestions(questionCount);
+    questionNumb++;
+    countQuestion(questionNumb);
+    nextBtn.classList.remove("active");
+  } else {
+    showresultBox();
+  }
+};
+const optionList = document.querySelector(".option-list");
+//getting quest and options from array
+function showQuestions(index) {
+  const questionText = document.querySelector(".question-text");
+  questionText.textContent = `${questions[index].numb}.${questions[index].question}`;
+  let optiontag = `<div class="option"><span>${questions[index].options[0]}</span></div>
+  <div class="option"><span>${questions[index].options[1]}</span></div>
+  <div class="option"><span>${questions[index].options[2]}</span></div>
+  <div class="option"><span>${questions[index].options[3]}</span></div>`;
+  optionList.innerHTML = optiontag;
+  const option = document.querySelectorAll(".option");
+  for (let i = 0; i < option.length; i++) {
+    option[i].setAttribute("onclick", "optionselected(this)");
+  }
+}
+
+function optionselected(answer) {
+  let useranswer = answer.textContent;
+  let correctAnswer = questions[questionCount].answer;
+  let alloptions = optionList.children.length;
+  if (useranswer == correctAnswer) {
+    answer.classList.add("correct");
+    userscore += 1;
+    headerscore();
+  } else {
+    answer.classList.add("incorrect");
+    //if answer is incorrect auto select correct one
+    for (let i = 0; i < alloptions; i++) {
+      if (optionList.children[i].textContent == correctAnswer) {
+        optionList.children[i].setAttribute("class", "option correct");
+      }
+    }
+  }
+  //user selected disable the rest
+  for (let i = 0; i < alloptions; i++) {
+    optionList.children[i].classList.add("disabled");
+  }
+  nextBtn.classList.add("active");
+}
+function countQuestion(index) {
+  const questionTotal = document.querySelector(".question-total");
+  questionTotal.textContent = `${index} of ${questions.length} Questions`;
+}
+
+function headerscore() {
+  const headerscoretext = document.querySelector(".header-score");
+  headerscoretext.textContent = `Score: ${userscore} / ${questions.length}`;
+}
+
+function showresultBox() {
+  quizbox.classList.remove("active");
+  resultbox.classList.add("active");
+  const scoretext = document.querySelector(".score-text");
+  scoretext.textContent = `your Score: ${userscore} / ${questions.length}`;
+  const circularProgress = document.querySelector(".circular-progress");
+  const Progressvalue = document.querySelector(".progress-value");
+  let progressstartvalue = -1;
+  let progressendvalue = (userscore / questions.length) * 100;
+  let speed = 20;
+  let progress = setInterval(() => {
+    progressstartvalue++;
+    Progressvalue.textContent = `${progressstartvalue}%`;
+    circularProgress.style.background = `conic-gradient(#0062c4 ${
+      progressstartvalue * 3.6
+    }deg, rgba(255, 255, 255, 0.1) 0deg)`;
+    if (progressstartvalue == progressendvalue) {
+      clearInterval(progress);
+    }
+  }, speed);
+}
