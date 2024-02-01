730d7ce8fe79811b2de816264464cbfb10b3a48a
ashir245 <117296966+ashir245@users.noreply.github.com>
2024-02-01 15:50:41 +0300

    modified interactive

--- javascript_projects/interactive_image_gallery/index.js
+++ javascript_projects/interactive_image_gallery/index.js
@@ -1,18 +1,26 @@
 let images = document.querySelectorAll("img");
 let wrapper = document.getElementById("wrapper");
-
 let imgwrapper = document.getElementById("fullimg");
-
 let close = document.querySelector("span");
+
+let isImageOpen = false; // Flag to track if the image is currently open
 
 images.forEach((img, index) => {
   img.addEventListener("click", () => {
-    openmodel(`images/img${index}.jpg`);
+    if (isImageOpen) {
+      wrapper.style.display = "none"; // Close the image if it's already open
+    } else {
+      openmodel(`images/img${index}.jpg`);
+    }
+    isImageOpen = !isImageOpen; // Toggle the flag
   });
 });
+
 close.addEventListener("click", () => {
   wrapper.style.display = "none";
+  isImageOpen = false; // Reset the flag when the close button is clicked
 });
+
 function openmodel(pic) {
   wrapper.style.display = "flex";
   imgwrapper.src = pic;
